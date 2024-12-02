974ebc3be4989af04189150252ca7933f378dd21
Achu-kappithan <achukappithan4236@gmail.com>
2024-12-02 09:04:45 +0530

    Add some aditional validation to checkout address forms.

--- utils/pricehelper.js
+++ utils/pricehelper.js
@@ -1,17 +1,3 @@
-// exports.getAvailablePrice = function(sizes) {
-//     for (const size of sizes) {
-//       if (size.stock > 0) {
-//         return size.Salesprice;
-//       }
-//     }
-//     return null;
-//   };
-  
-//   exports.formatPrice = function(price) {
-//     return price 
-//       ? price.toLocaleString('en-IN', { style: 'currency', currency: 'INR' }) 
-//       : 'Unavailable';
-//   };
 
 
 const price = function(size) {
